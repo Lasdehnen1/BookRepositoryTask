43fad906a40fba3b960ddbff51ca1e56f5f4af5f
Evgenii.Pichugov <eugepic@yandex.ru>
2024-05-14 16:13:48 +0300

    Initial commit

---
+++ src/test/java/com/example/bookdatabase/BookDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDataBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
